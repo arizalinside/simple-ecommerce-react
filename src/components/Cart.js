163e99b7e7f7c25c7ac130acb6643fba6de9a270
arizalinside <arizalinc@gmail.com>
2021-10-05 17:35:26 +0700

    Import cart list and cart total

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,6 +3,8 @@ import { Title } from "./Title";
 import CartColumns from "./CartColumns";
 import EmptyCart from "./EmptyCart";
 import { ProductConsumer } from "../Context";
+import CartList from "./CartList";
+import CartTotal from "./CartTotal";
 
 class Cart extends Component {
   render() {
@@ -16,6 +18,8 @@ class Cart extends Component {
                 <React.Fragment>
                   <Title name="your" title="cart" />
                   <CartColumns />
+                  <CartList value={value} />
+                  <CartTotal value={value} />
                 </React.Fragment>
               );
             } else {
