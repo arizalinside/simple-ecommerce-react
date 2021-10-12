d517a0fd3ea901f883214a438d254363890bc674
Arizal Arizal <arizalinc@gmail.com>
2021-10-13 00:13:11 +0700

    Change button style

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,16 +4,16 @@ export const ButtonContainer = styled.button`
   text-transform: capitalize;
   font-size: 1.4rem;
   background: transparent;
-  border: 0.05rem solid var(--lightBlue);
-  border-color: var(--lightBlue);
-  color: var(--lightBlue);
+  border: 0.15rem solid var(--lightDark);
+  border-color: var(--lightDark);
+  color: var(--lightDark);
   border-radius: 0.5rem;
   padding: 0.3rem 0.5rem;
   cursor: pointer;
   margin: 0.2rem 0.5rem 0.2rem 0;
   transition: all 0.35s ease-in-out;
   &:hover {
-    background: var(--lightBlue);
-    color: var(--mainBlue);
+    background: var(--mainDark);
+    color: var(--mainWhite);
   }
 `;
