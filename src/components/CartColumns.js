335fd17e0dde610eb0a72148d2de36a36ffc447a
Arizal Arizal <arizalinc@gmail.com>
2021-10-13 00:12:48 +0700

    Change container-fluid to container

--- src/components/CartColumns.js
+++ src/components/CartColumns.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function CartColumns() {
   return (
     <div>
-      <div className="container-fluid text-center d-none d-lg-block">
+      <div className="container text-center d-none d-lg-block">
         <div className="row">
           <div className="col-10 mx-auto col-lg-2">
             <p className="text-uppercase">products</p>
