da0f4b7c55fade34d725fb8eb33b524efb8244af
Arizal Arizal <arizalinc@gmail.com>
2021-10-13 00:03:27 +0700

    add class container

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,11 +2,13 @@ import React from "react";
 
 export const Title = ({ name, title }) => {
   return (
-    <div className="row">
-      <div className="col-10 mx-auto my-2 text-center text-title">
-        <h1 className="text-capitalize font-weigth-bold">
-          {name} <strong className="text-blue">{title}</strong>
-        </h1>
+    <div className="container">
+      <div className="row">
+        <div className="col mx-auto my-3 text-center text-title">
+          <h1 className="text-capitalize font-weigth-bold">
+            {name} <strong className="text-blue">{title}</strong>
+          </h1>
+        </div>
       </div>
     </div>
   );
