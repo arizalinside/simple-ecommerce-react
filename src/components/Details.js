1c2f11f3ab85607a1301200414358ae71e48b274
Arizal Arizal <arizalinc@gmail.com>
2021-10-13 00:08:46 +0700

    Just improve my code to be readable

--- src/components/Details.js
+++ src/components/Details.js
@@ -8,15 +8,8 @@ class Details extends Component {
     return (
       <ProductConsumer>
         {(value) => {
-          const {
-            id,
-            company,
-            img,
-            info,
-            price,
-            title,
-            inCart,
-          } = value.detailProduct;
+          const { id, company, img, info, price, title, inCart } =
+            value.detailProduct;
           return (
             <div className="container py-5">
               <div className="row">
